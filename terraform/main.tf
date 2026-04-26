fbaa604daad7320e8326ff26c972886766bf30d3
Amoda <amodarivindu@gmail.com>
2026-04-26 21:37:36 +0530

    terraform aws config modify2

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,10 +12,57 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route to internet
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+# Associate subnet with route table
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# EC2 instances
 resource "aws_instance" "app_server" {
-  count         = 2   # 🔥 multiple EC2s
+  count         = 2
   ami           = "ami-098e39bafa7e7303d"
   instance_type = "t2.micro"
+
+  subnet_id                   = aws_subnet.main.id
+  associate_public_ip_address = true
 
   tags = {
     Name = "AppServer-${count.index}"
